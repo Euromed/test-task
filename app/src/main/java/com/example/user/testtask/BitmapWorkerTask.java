30419b13aed14cca3f5f11b2bada22760751ae10
euromed <24euromed@mail.ru>
2016-10-29 17:33:01 +0700

    3

--- app/src/main/java/com/example/user/testtask/BitmapWorkerTask.java
+++ app/src/main/java/com/example/user/testtask/BitmapWorkerTask.java
@@ -3,9 +3,11 @@ package com.example.user.testtask;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.ViewDebug;
 import android.widget.ImageView;
@@ -20,63 +22,137 @@ import java.net.URL;
  */
 class BitmapWorkerTask extends AsyncTask<Object, Void, Bitmap> {
     private final WeakReference<ImageView> imageViewReference;
-    private final Resources resources;
-    private int data = 0;
-    private String urlData;
+    private final ImageLoader imageLoader;
 
     static class AsyncDrawable extends BitmapDrawable {
         private final WeakReference<BitmapWorkerTask> bitmapWorkerTaskReference;
+        private BitmapWorkerTask task;
+        private int width = -1;
+        private int height = -1;
 
         public AsyncDrawable(Resources res, Bitmap bitmap,
                              BitmapWorkerTask bitmapWorkerTask) {
             super(res, bitmap);
             bitmapWorkerTaskReference =
                     new WeakReference<BitmapWorkerTask>(bitmapWorkerTask);
+            task = bitmapWorkerTask;
         }
 
         public BitmapWorkerTask getBitmapWorkerTask() {
             return bitmapWorkerTaskReference.get();
         }
-    }
 
-    public static void loadBitmap(int resId, ImageView imageView) {
-        loadBitmap(resId, null, imageView);
-    }
+        void tryLoad(int width, int height) {
+            if (task != null && width > 0 && height > 0) {
+                task.execute(width, height);
+                task = null;
+            }
+            this.width = width;
+            this.height = height;
+        }
 
-    public static void loadBitmap(String url, ImageView imageView) {
-        loadBitmap(-1, url, imageView);
-    }
-
-    public static void loadBitmap(int resId, String url, ImageView imageView) {
-        if (cancelPotentialWork(resId, url, imageView)) {
-            final BitmapWorkerTask task = new BitmapWorkerTask(imageView);
-            final AsyncDrawable asyncDrawable =
-                    new AsyncDrawable(imageView.getResources(), null, task);
-            imageView.setImageDrawable(asyncDrawable);
-
-            int w = imageView.getWidth();
-            int h = imageView.getHeight();
-            Log.i("BWT", "Load bitmap - view size (" + w + ", " + h + ")\n");
-
-            task.execute(resId, url, imageView.getWidth(), imageView.getHeight());
+        @Override
+        protected void onBoundsChange(Rect bounds) {
+            super.onBoundsChange(bounds);
+            int w = bounds.width();
+            int h = bounds.height();
+            if (w != width || h != height) {
+                tryLoad(w, h);
+            }
         }
     }
 
-    public static boolean cancelPotentialWork(int data, String url, ImageView imageView) {
+    interface ImageLoader {
+        public Bitmap load(int width, int height);
+    }
+
+    public static void loadBitmap(ImageLoader imageLoader, ImageView imageView) {
+        if (cancelPotentialWork(imageLoader, imageView)) {
+            final BitmapWorkerTask task = new BitmapWorkerTask(imageLoader, imageView);
+            final AsyncDrawable asyncDrawable =
+                    new AsyncDrawable(imageView.getResources(), null, task);
+            imageView.setImageDrawable(asyncDrawable);
+            asyncDrawable.tryLoad(imageView.getWidth(), imageView.getHeight());
+        }
+    }
+
+    public static void loadBitmap(final String url, ImageView imageView) {
+        ImageLoader imageLoader = new ImageLoader() {
+            String urlData = url;
+            @Override
+            public Bitmap load(int width, int height) {
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 1;
+                options.outWidth = width;
+                options.outHeight = height;
+                Bitmap bitmap = null;
+                InputStream is = null;
+                try {
+                    HttpURLConnection conn = null;
+                    for (int i = 0; i < 10; ++i) {
+                        URL url = new URL(urlData);
+                        conn = (HttpURLConnection) url.openConnection();
+                        conn.setReadTimeout(10000 /* milliseconds */);
+                        conn.setConnectTimeout(15000 /* milliseconds */);
+                        conn.setRequestMethod("GET");
+                        conn.setDoInput(true);
+                        conn.setInstanceFollowRedirects(true);
+                        conn.connect();
+                        int response = conn.getResponseCode();
+                        Log.i("BWT", "The response for image URL is: " + response);
+                        if (response / 100 == 3) {
+                            urlData = conn.getHeaderField("Location");
+                            Log.i("BWT", "The response redirect to: " + urlData);
+                            continue;
+                        }
+                        break;
+                    }
+                    is = conn.getInputStream();
+                    bitmap = BitmapFactory.decodeStream(is, null, options);
+                    Log.i("BWT", "Decoded bitmap - " + bitmap.getWidth() + "x" + bitmap.getHeight() + "\n");
+                }
+                catch (Exception e) {}
+                finally {
+                    if (is != null) {
+                        try {
+                            is.close();
+                        }
+                        catch (Exception e) {}
+                    }
+                }
+                return bitmap;
+            }
+        };
+        loadBitmap(imageLoader, imageView);
+    }
+
+    public static void loadBitmap(final int resId, final ImageView imageView) {
+        ImageLoader imageLoader = new ImageLoader() {
+            final Resources resources = imageView.getResources();
+            public Bitmap load(int width, int height) {
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 1;
+                options.outWidth = width;
+                options.outHeight = height;
+                return BitmapFactory.decodeResource(resources, resId, options);
+            }
+        };
+        loadBitmap(imageLoader, imageView);
+    }
+
+    public static boolean cancelPotentialWork(ImageLoader imageLoader, ImageView imageView) {
         final BitmapWorkerTask bitmapWorkerTask = getBitmapWorkerTask(imageView);
 
         if (bitmapWorkerTask != null) {
-            final int bitmapData = bitmapWorkerTask.data;
-            final String bitmapUrlData = bitmapWorkerTask.urlData;
+            final ImageLoader workerImageLoader = bitmapWorkerTask.imageLoader;
             // If bitmapData is not yet set or it differs from the new data
-            if (bitmapData == 0 || bitmapData != data ||
-                    bitmapUrlData != url && (bitmapUrlData == null || url == null || !bitmapUrlData.equals(url))) {
-                // Cancel previous task
-                bitmapWorkerTask.cancel(true);
-            } else {
+            if (workerImageLoader == imageLoader) {
+//-----!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                 // The same work is already in progress
-                return false;
+                return (false);
             }
+            // Cancel previous task
+            bitmapWorkerTask.cancel(true);
         }
         // No task associated with the ImageView, or an existing task was cancelled
         return true;
@@ -93,74 +169,23 @@ class BitmapWorkerTask extends AsyncTask<Object, Void, Bitmap> {
         return null;
     }
 
-    public BitmapWorkerTask(ImageView imageView) {
+    public BitmapWorkerTask(ImageLoader imageLoader, ImageView imageView) {
         // Use a WeakReference to ensure the ImageView can be garbage collected
         imageViewReference = new WeakReference<ImageView>(imageView);
-        resources = imageView.getResources();
+        this.imageLoader = imageLoader;
     }
 
     // Decode image in background.
     @Override
     protected Bitmap doInBackground(Object... params) {
-        data = (Integer)params[0];
-        urlData = (String)params[1];
-        int width = (Integer)params[2];
-        int height = (Integer)params[3];
-        Bitmap bitmap = null;
-        InputStream is = null;
-        Log.i("BWT", "Load bitmap - " + (urlData == null ? data : urlData) + "\n");
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 1;
-        options.outWidth = width;
-        options.outHeight = height;
-        if (urlData == null) {
-            bitmap = BitmapFactory.decodeResource(resources, data, options);
-        }
-        else {
-            try {
-                HttpURLConnection conn = null;
-                for (int i = 0; i < 10; ++i) {
-                    URL url = new URL(urlData);
-                    conn = (HttpURLConnection) url.openConnection();
-                    conn.setReadTimeout(10000 /* milliseconds */);
-                    conn.setConnectTimeout(15000 /* milliseconds */);
-                    conn.setRequestMethod("GET");
-                    conn.setDoInput(true);
-                    conn.setInstanceFollowRedirects(true);
-                    conn.connect();
-                    int response = conn.getResponseCode();
-                    Log.i("BWT", "The response for image URL is: " + response);
-                    if (response / 100 == 3) {
-                        urlData = conn.getHeaderField("Location");
-                        Log.i("BWT", "The response redirect to: " + urlData);
-                        continue;
-                    }
-                    break;
-                }
-                is = conn.getInputStream();
-                bitmap = BitmapFactory.decodeStream(is, null, options);
-                Log.i("BWT", "Decoded bitmap - " + bitmap.getWidth() + "x" + bitmap.getHeight() + "\n");
-            }
-            catch (Exception e) {}
-            finally {
-                if (is != null) {
-                    try {
-                        is.close();
-                    }
-                    catch (Exception e) {}
-                }
-            }
-        }
-        return bitmap;
+        int width = (Integer)params[0];
+        int height = (Integer)params[1];
+        return imageLoader.load(width, height);
     }
 
     @Override
     protected void onPostExecute(Bitmap bitmap) {
-        if (isCancelled()) {
-            bitmap = null;
-        }
-
-        if (imageViewReference != null && bitmap != null) {
+        if (!isCancelled() && imageViewReference != null && bitmap != null) {
             final ImageView imageView = imageViewReference.get();
             final BitmapWorkerTask bitmapWorkerTask =
                     getBitmapWorkerTask(imageView);
