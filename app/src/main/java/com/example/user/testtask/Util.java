cb6a6ba90f782a735879abd2ca10d35a172049f4
euromed <24euromed@mail.ru>
2016-11-17 06:33:16 +0700

    2

--- app/src/main/java/com/example/user/testtask/Util.java
+++ app/src/main/java/com/example/user/testtask/Util.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 
+import com.ibm.icu.util.*;
+import com.ibm.icu.text.*;
+
+import java.text.ParsePosition;
+
 /**
  * Created by const on 04.11.2016.
  */
@@ -13,5 +18,25 @@ public class Util {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setDataAndType(Uri.parse(url), "image/*");
         activity.startActivity(intent);
+    }
+
+    public static String formatDateTime(Calendar v) {
+        DateFormat dateFormat = SimpleDateFormat.getDateTimeInstance();
+        dateFormat.setCalendar((Calendar)v.clone());
+        return dateFormat.format(v.getTime());
+    }
+
+    public static Calendar parseDateTime(String src) {
+        Calendar rv = new GregorianCalendar();
+        DateFormat dateFormat = SimpleDateFormat.getDateTimeInstance();
+
+        ((SimpleDateFormat)dateFormat).parse(src, rv, new ParsePosition(0));
+        try {
+            dateFormat.parse(src);
+            rv = dateFormat.getCalendar();
+        }
+        catch (ParseException e) {
+        } TimeZone tz;
+        return rv;
     }
 }
