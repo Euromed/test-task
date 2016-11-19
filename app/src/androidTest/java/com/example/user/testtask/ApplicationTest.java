7731fdc97574c26c5e660d02e1b47eea381c5bb0
euromed <24euromed@mail.ru>
2016-11-19 07:04:20 +0700

    2

--- app/src/androidTest/java/com/example/user/testtask/ApplicationTest.java
+++ app/src/androidTest/java/com/example/user/testtask/ApplicationTest.java
@@ -2,6 +2,10 @@ package com.example.user.testtask;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.text.SpannableString;
+import android.util.Log;
+
+import com.ibm.icu.util.GregorianCalendar;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,5 +13,11 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+        testFormatDateTime();
+    }
+
+    public void testFormatDateTime() {
+        SpannableString v = Util.formatDateTimeTimeZone(GregorianCalendar.getInstance());
+        Log.i("TFDT", v.toString());
     }
 }
--- app/src/main/java/com/example/user/testtask/Util.java
+++ app/src/main/java/com/example/user/testtask/Util.java
@@ -3,11 +3,17 @@ package com.example.user.testtask;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.SubscriptSpan;
 
+import com.ibm.icu.text.DateFormat;
+import com.ibm.icu.text.SimpleDateFormat;
 import com.ibm.icu.util.*;
 import com.ibm.icu.text.*;
 
-import java.text.ParsePosition;
+import java.text.*;
 
 /**
  * Created by const on 04.11.2016.
@@ -20,10 +26,13 @@ public class Util {
         activity.startActivity(intent);
     }
 
-    public static String formatDateTime(Calendar v) {
+    public static SpannableString formatDateTimeTimeZone(Calendar v) {
         DateFormat dateFormat = SimpleDateFormat.getDateTimeInstance();
-        dateFormat.setCalendar((Calendar)v.clone());
-        return dateFormat.format(v.getTime());
+        String formattedDateTime = dateFormat.format(v);
+        int dateTimeLength = formattedDateTime.length();
+        SpannableString rv = new SpannableString(formattedDateTime + " " + v.getTimeZone().getDisplayName());
+        rv.setSpan(new SubscriptSpan(), dateTimeLength, rv.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        return rv;
     }
 
     public static Calendar parseDateTime(String src) {
